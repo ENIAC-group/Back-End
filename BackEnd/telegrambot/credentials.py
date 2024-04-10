3e6c9812141bb73ec0aa917e2c0150eee63f0e4e
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-04-10 22:02:51 +0330

    m

--- BackEnd/telegrambot/credentials.py
+++ BackEnd/telegrambot/credentials.py
@@ -1,4 +1,5 @@
 TOKEN = '6800635126:AAGV8Ev4Wf4qsq4lPa-Vm-dcYNsoQ19h1So'
 TELEGRAM_API_URL = f'https://api.telegram.org/bot{TOKEN}/'
 
-
+# curl -X POST "https://api.telegram.org/bot6800635126:AAGV8Ev4Wf4qsq4lPa-Vm-dcYNsoQ19h1So/setWebhook?url=84bf-5-62-211-211.ngrok-free.app/webhook"
+# https://github.com/Ali-Toosi/django-tgbot
--- BackEnd/telegrambot/views.py
+++ BackEnd/telegrambot/views.py
@@ -1,22 +1,43 @@
 from django.shortcuts import render
+import os
+from .credentials import TELEGRAM_API_URL , TOKEN
 
-# Create your views here.
-import json
-import requests
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from .credentials import TELEGRAM_API_URL
+import telebot
 
-@csrf_exempt
-def telegram_bot(request):
-  if request.method == 'POST':
-    message = json.loads(request.body.decode('utf-8'))
-    chat_id = message['message']['chat']['id']
-    text = message['message']['text']
-    send_message("sendMessage", {
-      'chat_id': f'your message {text}'
-    })
-  return HttpResponse('ok')
+# BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-def send_message(method, data):
-  return requests.post(TELEGRAM_API_URL + method, data)
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+
+bot.infinity_polling()
+
+    # Create your views here.
+# import json
+# import requests
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+
+# @csrf_exempt
+# def telegram_bot(request):
+#   if request.method == 'POST':
+#     message = json.loads(request.body.decode('utf-8'))
+#     chat_id = message['message']['chat']['id']
+#     text = message['message']['text']
+#     send_message("sendMessage", {
+#       'chat_id': f'your message {text}'
+#     })
+#   return HttpResponse('ok')
+
+# def send_message(method, data):
+#   return requests.post(TELEGRAM_API_URL + method, data)
