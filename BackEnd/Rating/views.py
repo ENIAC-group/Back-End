e9fd465d3f4b4bd027e75b074229d9d642715f00
Asma Hamid <asmahamid1382@gmail.com>
2024-05-15 22:06:20 +0330

    fix/Rating

--- BackEnd/Rating/views.py
+++ BackEnd/Rating/views.py
@@ -5,12 +5,31 @@ from .Serializer import  RatingSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 class RatingViewSet(APIView):
     serializer_class = RatingSerializer
     permission_classes = [IsAuthenticated]
     def post(self, request):
         serializer = RatingSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        if serializer.is_valid():
+            pationt_id = request.user.id
+            psychiatrist = serializer.validated_data['psychiatrist']
+
+            if Rating.objects.filter(pationt=pationt_id, psychiatrist=psychiatrist).exists():
+                return Response({'error': 'You have already rated this psychiatrist.'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            pationt=Pationt.objects.get(pk=pationt_id)
+
+            doctor_rate=Rating.objects.create(
+                psychiatrist=psychiatrist,
+                pationt=pationt,
+                rating=serializer.validated_data['rating'],
+                comments=serializer.validated_data['comments'],
+
+            )
+            doctor_rate.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
