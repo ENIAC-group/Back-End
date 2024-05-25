2a062196553742165b0d858a52c3d6d0dccab603
Asma Hamid <asmahamid1382@gmail.com>
2024-05-25 16:15:10 +0330

    fix/fb_DoctorPanel/5/25/2024

--- BackEnd/Doctorpanel/urls.py
+++ BackEnd/Doctorpanel/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path("get_rating/" , DoctorPanelView.as_view({'get':'get_rating'}) , name="GetRating") , 
     path("ThisWeekResevations/" , DoctorPanelView.as_view({'get':'ThisWeekResevations'}) , name="ReservationList") , 
     path("NextWeekReservations/" , DoctorPanelView.as_view({'get':'NextWeekReservations'}) , name="ReservationList2") , 
-    path('doctor/free-time/', DoctorPanelView.as_view({'post':'PostFreeTime'})),
+    path('doctor/post-free-time/', DoctorPanelView.as_view({'post':'PostFreeTime'})),
+    path('doctor/get-free-time/', DoctorPanelView.as_view({'get':'GetAllFreeTime'})),
 
 
 ]
