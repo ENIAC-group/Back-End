4a1d95a5a812a8490004174e5deeadb38cce3f96
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-06-05 08:51:28 +0330

    fix/

--- BackEnd/Profile/views.py
+++ BackEnd/Profile/views.py
@@ -2,16 +2,23 @@ from rest_framework import viewsets, serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Profile
-from Profile.serializer import DoctorProfileSerializer
-from rest_framework.permissions import IsAuthenticated
 
+class DoctorProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
 
-class RecomendationSysView(viewsets.ModelViewSet):
+class DoctorProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = DoctorProfileSerializer
-    permission_classes = [IsAuthenticated]
-    @action(detail=False, methods=['get'])
-    def get_recomended_doctors(self, request):
-        pass
 
+    @action(detail=False, methods=['get'])
+    def filter_by_profile_type(self, request):
+        profile_type = request.query_params.get('profile_type')
+        if profile_type:
+            queryset = self.queryset.filter(profile_type=profile_type)
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            return Response({"error": "profile_type parameter is required"}, status=400)
 
