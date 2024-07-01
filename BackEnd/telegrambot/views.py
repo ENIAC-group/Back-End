08d9b7d9ac327e9099185e08eb4de4381d710ba3
zahra alizadeh <zahra121281alizadeh@gmail.com>
2024-07-01 07:11:14 +0330

    tel bot

--- BackEnd/telegrambot/views.py
+++ BackEnd/telegrambot/views.py
@@ -167,11 +167,11 @@ def handle_other_commands(chat_id ,text  ):
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-class SetWebhookView(APIView):
+from rest_framework import viewsets
+class SetWebhookView(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request):
+    def set_webhook(self, request):
         if request.user.role == 'admin':
             response = requests.get(url=WEB_HOOK_URL)
             return Response(response.content)
