d9d51f8479b2e9f00ac1a298317a676c352ccd2c
Asma Hamid <asmahamid1382@gmail.com>
2024-07-03 14:16:03 +0330

    fix/fb_GoogleMeet/Signal/7/3/2024

--- BackEnd/reservation/signals.py
+++ BackEnd/reservation/signals.py
@@ -1,11 +1,21 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from reservation.models import Reservation
+from .models import Reservation
 from GoogleMeet.views import GoogleMeetAPIView  
+from rest_framework.test import APIRequestFactory  
 
 @receiver(post_save, sender=Reservation)
-def create_virtual_meeting(sender, instance, created, **kwargs):
-    if created and instance.type == 'مجازی': 
-        google_meet_api = GoogleMeetAPIView()
-        google_meet_api.post(request=None ,data={"reservation_id": instance.id})
-# ///
+def create_google_meet_link(sender, instance, created, **kwargs):
+    if created and instance.type == 'مجازی':
+        factory = APIRequestFactory()
+        request = factory.post('/api/googlemeet/', {
+            'reservation_id': instance.id
+        }, format='json')
+        
+        google_meet_api_view = GoogleMeetAPIView.as_view()
+        response = google_meet_api_view(request)
+        
+        if response.status_code == 201:
+            print('Google Meet link created successfully.')
+        else:
+            print('Failed to create Google Meet link.')
